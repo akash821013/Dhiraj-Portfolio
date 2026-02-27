65e14c4ae0fc75e4fa998d7e835b0da655ef56b2
Dhiraj kumar <dhirajk585@gmail.com>
2026-02-27 17:35:07 +0530

    chngpic

--- src/app/application/page-modules/Home/home-page/home-page.ts
+++ src/app/application/page-modules/Home/home-page/home-page.ts
@@ -1,5 +1,5 @@
 import { UpperCasePipe } from '@angular/common';
-import { Component, AfterViewInit, Inject, PLATFORM_ID, Injectable} from '@angular/core';
+import { Component, AfterViewInit, OnInit, Inject, PLATFORM_ID, Injectable } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { Router, RouterLink } from '@angular/router';
 import { isPlatformBrowser } from '@angular/common';
@@ -10,13 +10,14 @@ import { isPlatformBrowser } from '@angular/common';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [UpperCasePipe, MatIconModule,RouterLink],
+  imports: [UpperCasePipe, MatIconModule, RouterLink],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
 export class HomePage implements AfterViewInit {
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  isDarkMode: boolean = false;
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
 
   async ngAfterViewInit(): Promise<void> {
     if (isPlatformBrowser(this.platformId)) {
@@ -28,18 +29,36 @@ export class HomePage implements AfterViewInit {
         offset: 120,
       });
 
-        setTimeout(() => {
+      setTimeout(() => {
         AOS.default.refresh();
       }, 500);
     }
   }
+
+  downloadResume() {
+    const link = document.createElement('a');
+    link.href = 'assets/resume.pdf';
+    link.download = 'Dhiraj_Kumar_Resume.pdf';
+    link.click();
+  }
+
+// toggleTheme(): void {
+//   document.body.classList.toggle('dark-theme');
+// }
+
+toggleTheme(): void {
+  console.log("Toggle clicked");
+  document.body.classList.toggle('dark-theme');
+}
+  
+
 }
 
 // ---------------------- Reusable Service ------------------ //
 
 @Injectable({ providedIn: 'root' })
 export class NavigationService {
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   goBack() {
     this.router.navigateByUrl('/');
